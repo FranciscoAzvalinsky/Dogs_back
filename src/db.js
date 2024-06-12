88b4c730c7eab497eb5564692cb4b7f0ad2c844c
Francisco Azvalinsky <frankyazva@gmail.com>
2024-06-12 02:32:59 -0300

    Intento deploy numero 8

--- src/db.js
+++ src/db.js
@@ -12,6 +12,8 @@ const { DB_URL } = require("./config.js");
 const sequelize = new Sequelize(`${DB_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialect: 'postgres',
+  protocol: 'postgres',
 });
 
 const basename = path.basename(__filename);
