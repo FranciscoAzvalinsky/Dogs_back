a6aac59c410c6c4d2fd2087379745c38b92e19c1
Francisco Azvalinsky <frankyazva@gmail.com>
2024-06-12 15:51:01 -0300

    Intento deploy numero 24

--- src/config.js
+++ src/config.js
@@ -7,8 +7,9 @@ const DB_USER = process.env.POSTGRES_USER || "postgres";
 const DB_PASSWORD = process.env.POSTGRES_PASSWORD || "admin";
 const DB_HOST = process.env.POSTGRES_HOST || "localhost";
 const DB_NAME = process.env.POSTGRES_DATABASE || "dogs";
+const DB_POSTGRES_URL = process.env.POSTGRES_URL || "postgres://"
 //const DB_PORT = process.env.DB_PORT || 3001;
-const DB_URL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+const DB_URL = `${DB_POSTGRES_URL}${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
 module.exports = {
   PORT,
